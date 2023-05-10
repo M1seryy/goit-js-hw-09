b038915f7a10649d57a938208bf316e3b9452852
M1seryy <62029606+M1seryy@users.noreply.github.com>
2023-05-10 12:34:37 +0300

    task 1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let intervalId = null;
+stopBtn.disabled = true;
+
+startBtn.addEventListener('click', () => {
+  stopBtn.disabled = false;
+  startBtn.disabled = true;
+
+  intervalId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+});
+stopBtn.addEventListener('click', () => {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(intervalId);
+});
